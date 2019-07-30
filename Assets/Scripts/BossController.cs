d27092433606482005830fcc788aa1a77d39818e
leo.champion <leo.champion@protonmail.com>
2019-07-31 00:06:25 +0200

    Quick clean up of BossController.cs

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public int maxHealth = 100;
-    public int health { get { return currentHealth; } }
-    public float laserBlockTime = 1f;
-    private float blockTimer = 0.8f;
+
     public GameObject laserPrefab;
     public GameObject monsterPrefab;
     public GameObject handFallPrefab;
+    public int health { get { return currentHealth; } }
+
+    Animator animator;
+    Rigidbody2D rigidBody2D;
+    float startGameTimer;
+    float speed = 5.0f;
+    float laserBlockTime = 1f;
+    float blockTimer = 0.8f;
     float invincibleTimer;
     float deathTimer;
-    private float laserTimer;
-    private float monsterTimer;
-    private float handFallTimer;
+    float laserTimer;
+    float monsterTimer;
+    float handFallTimer;
     int currentHealth;
-    // float invincibleTimer;
-    Animator animator;
-
+    int maxHealth = 100;
     bool dead;
-    Rigidbody2D rigidBody2D;
-    Vector2 lookDirection = new Vector2(1, 0);
-    float startGameTimer;
 
     void Start()
     {
@@ -35,114 +34,73 @@ public class BossController : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    void Move(Vector2 mvect)
+    {
+        if (blockTimer < 0f)
+        {
+            Vector2 position = rigidBody2D.position;
+            position = position + mvect * speed * Time.deltaTime;
+            rigidBody2D.MovePosition(position);
+        }
+    }
+
     void Update()
     {
         invincibleTimer -= Time.deltaTime;
         deathTimer -= Time.deltaTime;
         startGameTimer -= Time.deltaTime;
-        if (startGameTimer > 0f) {
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
-            Vector2 move = new Vector2(horizontal, vertical);
-
-            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-            {
-                lookDirection.Set(move.x, move.y);
-                lookDirection.Normalize();
-            }
-            // Debug.Log(lookDirection);
-
-            // animator.SetFloat ("x", lookDirection.x);
-            // animator.SetFloat ("y", lookDirection.y);
-            if (blockTimer == 0f)
-            {
-                Vector2 position = rigidBody2D.position;
-                position = position + move * speed * Time.deltaTime;
-                rigidBody2D.MovePosition(position);
-            }
-            return;
-        }
-        if (deathTimer > 0)
-        {
-            dead = true;
-            animator.SetBool("death", true);
-        }
+        laserTimer -= Time.deltaTime;
+        monsterTimer -= Time.deltaTime;
+        handFallTimer -= Time.deltaTime;
+        blockTimer -= Time.deltaTime;
+        if (dead)
+            UpdateDead();
         else
+            UpdateAlive();
+    }
+
+    private void UpdateDead()
+    {
+        if (deathTimer <= 0)
         {
-            if (invincibleTimer > 0)
-                animator.SetBool("invicible", true);
-            else
-                animator.SetBool("invicible", false);
-        }
-        if (deathTimer <= 0 && dead) {
             HeroController[] heros = GameObject.FindObjectsOfType<HeroController>();
-            if (heros.Length > 0) {
-                Destroy (heros[0].gameObject);
-            }
-            Destroy(gameObject);
-        }
-        if (!dead)
-        {
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
-
-            Vector2 move = new Vector2(horizontal, vertical);
-
-            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-            {
-                lookDirection.Set(move.x, move.y);
-                lookDirection.Normalize();
-            }
-            // Debug.Log(lookDirection);
-
-            // animator.SetFloat ("x", lookDirection.x);
-            // animator.SetFloat ("y", lookDirection.y);
-            if (blockTimer == 0f)
-            {
-                Vector2 position = rigidBody2D.position;
-                position = position + move * speed * Time.deltaTime;
-                rigidBody2D.MovePosition(position);
-            }
-
-            if (blockTimer > 0f)
-            {
-                blockTimer -= Time.deltaTime;
-                blockTimer = blockTimer >= 0f ? blockTimer : 0f;
-            }
-            if (laserTimer <= 0f && Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                Launch();
-                laserTimer = 1f;
-            }
-            if (monsterTimer <= 0f && vertical < 0)
-            {
-                Invoc();
-                monsterTimer = 5f;
-            }
-            if (handFallTimer <= 0f && Input.GetKeyDown(KeyCode.Mouse1))
-            {
-                HandFall();
-                handFallTimer = 1f;
-            }
-            laserTimer -= Time.deltaTime;
-            monsterTimer -= Time.deltaTime;
-            handFallTimer -= Time.deltaTime;
+            if (heros.Length > 0)
+                Destroy(heros[0].gameObject);
+            Destroy(gameObject); ;
         }
     }
 
-    // public void ChangeHealth (int amount) {
-    //     if (amount < 0) {
-    //         if (invincibleTimer != 0f)
-    //             return;
-    //         else
-    //             invincibleTimer = timeInvicible;
-    //     }
-    //     currentHealth = Mathf.Clamp (currentHealth + amount, 0, maxHealth);
-    //     Debug.Log (currentHealth + "/" + maxHealth);
-    //     UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
-    // }
+    private void UpdateAlive()
+    {
+        if (startGameTimer > 0f)
+            return;
+        if (invincibleTimer > 0)
+            animator.SetBool("invicible", true);
+        else
+            animator.SetBool("invicible", false);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-    void Launch()
+        Vector2 moveVect = new Vector2(horizontal, vertical);
+        Move(moveVect);
+        if (laserTimer <= 0f && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ShootLaser();
+            laserTimer = 1f;
+        }
+        if (monsterTimer <= 0f && vertical < 0)
+        {
+            Invoc();
+            monsterTimer = 5f;
+        }
+        if (handFallTimer <= 0f && Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            HandFall();
+            handFallTimer = 1f;
+        }
+    }
+
+    void ShootLaser()
     {
         GameObject laserObject = Instantiate(laserPrefab, rigidBody2D.position + new Vector2(0.08f, 0.1f), Quaternion.identity);
         LaserController lc = laserObject.GetComponent<LaserController>();
@@ -150,11 +108,7 @@ public class BossController : MonoBehaviour
         difference = -difference;
         float angle = Mathf.Atan2(-difference.x, difference.y) * Mathf.Rad2Deg;
         lc.Launch(angle);
-        // lc.Launch(angle + 100);
-        // lc.Launch(angle - 100);
         blockTimer = laserBlockTime;
-
-        // animator.SetTrigger ("Launch");
     }
 
     void Invoc()
@@ -176,6 +130,7 @@ public class BossController : MonoBehaviour
             if (currentHealth <= 0)
             {
                 deathTimer = 4f;
+                dead = true;
                 animator.SetBool("dead", true);
             }
         }
